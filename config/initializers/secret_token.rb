b87c35f798e78cc9145e5f8d363be0eea88db1ca
Julie Silvestri <julie.silvestri@gmail.com>
2014-07-02 10:16:07 -0400

    initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubyDroid::Application.config.secret_key_base = 'e624b297be5e2f5acf718aa14c60035456237aa1a0c21a17ed0f155aa94500667a55aedfd5fe8ca18d3d80fd1e6a56ebfdf5e7bb76e0058adf37fe62646b1dae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubyDroid::Application.config.session_store :cookie_store, key: '_ruby-droid_session'
